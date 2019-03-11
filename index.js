6e35ef8c1a9fa16af3c298e48b9bc050a67f58d6
Elliot Chen <lltchen1@gmail.com>
2019-03-11 21:39:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const drivers_arrays = ["Milo", "Otis", "Garfield"]
-
-appends driver "Odie" to the end of the drivers array ‣
-
-prependDriver
-
-removeLastDriver()
-
-
-removeFirstDriver()
+debugger
