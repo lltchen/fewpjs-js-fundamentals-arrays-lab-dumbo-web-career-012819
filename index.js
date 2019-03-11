00196521288c538d13d1676aabfaf243515460f2
Elliot Chen <lltchen1@gmail.com>
2019-03-11 21:38:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const drivers_arrays = ["Milo", "Otis", "Garfield"]
 
 appends driver "Odie" to the end of the drivers array ‣
-AssertionError: expected [] to have the same ordered members as [ 'Milo', 'Otis', 'Garfield', 'Odie' ]
-    at Context.<anonymous> (test/indexTest.js:16:40)
+
 prependDriver
-prepends a driver to the beginning of the drivers array ‣
-ReferenceError: prepend is not defined
-    at Context.<anonymous> (test/indexTest.js:22:16)
+
 removeLastDriver()
-removes the last driver from the drivers array ‣
-ReferenceError: removeLast is not defined
-    at Context.<anonymous> (test/indexTest.js:28:16)
+
+
 removeFirstDriver()
